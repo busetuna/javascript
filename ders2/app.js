755b334ff8720b06fb004efcac6dbc1d0b021d29
Buse <btuna4024@gmail.com>
2025-08-20 17:53:55 +0300

    started

---
+++ ders2/app.js
@@ -0,0 +1,23 @@
+document.writeln('Hello, World! HEYY <br>');
+document.writeln('Hello, World! HEYY');
+
+console.log('Hello, World! HEYY');
+console.clear();
+
+let a= 10;
+let b = 30;
+
+console.log("a + b = " , a+b);
+
+// ! window en dış katman , document ise iç katmandır.
+console.log(window);
+
+alert("Hey"); 
+// * pop up çıkar. 
+
+alert("İki sayının toplamı: " + (a+b));
+
+console.log(window.location.port);
+
+debugger; 
+// ! sources kısmında kodları tek tek ayıklamak için kullanılır.
